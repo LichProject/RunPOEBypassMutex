d6f16195e53240abacc2dffc664652e9f3bef078
LichProject <xlichproject@gmail.com>
2021-11-29 20:06:55 +0300

    Fixed NullReference exception, fixed ReadProcessMemory error, both args are combined into one.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-Allows you to run >2 path of exile windows on one pc. 
+Allows you to run >2 poe windows on one pc. 
 
 Doesn't inject anything into the process.
 
 ### Usage
 ```
-RunPOEBypassMutex.exe "C:\Path of Exile" PathOfExile.exe
+RunPOEBypassMutex.exe "C:\Path of Exile\PathOfExile.exe"
 ```
--- RunPOEBypassMutex/Program.cs
+++ RunPOEBypassMutex/Program.cs
@@ -23,10 +23,7 @@ namespace RunPOEBypassMutex
 
         static void Main(string[] args)
         {
-            var workingDir = args[0];
-            var executableName = args[1];
-
-            var path = Path.Combine(workingDir, executableName);
+            var path = args[0];
 
             if (!File.Exists(path) || !path.EndsWith(".exe"))
             {
@@ -35,61 +32,82 @@ namespace RunPOEBypassMutex
                 return;
             }
 
-            _process = Process.Start(
-                new ProcessStartInfo
+            try
+            {
+                _process = Process.Start(
+                    new ProcessStartInfo
+                    {
+                        WorkingDirectory = Path.GetDirectoryName(path), 
+                        FileName = path, 
+                        Arguments = "--nopatch"
+                    });
+
+                WaitForProcess();
+
+                var mainModule = _process.MainModule;
+                var handle = _process.Handle;
+
+                Threads.Suspend(_process);
+                Thread.Sleep(500);
+
+                int bytesRead = 0;
+                var size = mainModule.ModuleMemorySize;
+                var buffer = new byte[size];
+
+                try
                 {
-                    WorkingDirectory = workingDir,
-                    FileName = path,
-                    Arguments = "--nopatch"
-                });
-
-            var mainModule = _process.MainModule;
-            var handle = _process.Handle;
-
-            Threads.Suspend(_process);
-
-            int bytesRead = 0;
-            var size = mainModule.ModuleMemorySize;
-            var buffer = new byte[size];
-
-            if (!Native.ReadProcessMemory(handle, mainModule.BaseAddress, buffer, size, ref bytesRead))
-            {
-                ReportError("Unable to read the entire process memory.");
-                return;
-            }
-
-            Console.WriteLine("-\nChanging mutex names...\n-");
-            
-            // Set new mutex names.
-            var addresses = new IntPtr[MutexObjectNames.Length];
-            
-            for (int i = 0; i < MutexObjectNames.Length; i++)
-            {
-                var name = MutexObjectNames[i];
-                
-                if (!ChangeMutexName(name, buffer, out var baseAddress))
+                    if (!Native.ReadProcessMemory(handle, mainModule.BaseAddress, buffer, size, ref bytesRead))
+                    {
+                        ReportError("[-] Unable to read the entire process memory.");
+                        return;
+                    }
+                }
+                catch (Exception e)
+                {
+                    ReportError("[-] [Exception] Unable to read the entire process memory.");
                     return;
+                }
 
-                addresses[i] = baseAddress;
+                Console.WriteLine("-\n[+] Changing mutex names...\n-");
+
+                // Set new mutex names.
+                var addresses = new IntPtr[MutexObjectNames.Length];
+
+                for (int i = 0; i < MutexObjectNames.Length; i++)
+                {
+                    var name = MutexObjectNames[i];
+
+                    if (!ChangeMutexName(name, buffer, out var baseAddress))
+                        return;
+
+                    addresses[i] = baseAddress;
+                }
+
+                // Run poe.
+                Threads.Resume(_process);
+                Thread.Sleep(500);
+
+                Console.WriteLine("-\n[+] Restoring mutex names...\n-");
+
+                // Restore original mutex names.
+                for (int i = 0; i < MutexObjectNames.Length; i++)
+                {
+                    var name = MutexObjectNames[i];
+                    var address = addresses[i];
+
+                    if (!ChangeMutexName(address, name))
+                        return;
+                }
             }
-
-            // Run poe.
-            Threads.Resume(_process);
-            Thread.Sleep(1000);
-
-            Console.WriteLine("-\nRestoring mutex names...\n-");
-            
-            // Restore original mutex names.
-            for (int i = 0; i < MutexObjectNames.Length; i++)
+            catch (Exception e)
             {
-                var name = MutexObjectNames[i];
-                var address = addresses[i];
-                
-                if (!ChangeMutexName(address, name))
-                    return;
+                // Prevent .exe hangs.
+                ReportError("[-] " + e);
             }
-
-            _process.Dispose();
+            finally
+            {
+                _process?.Dispose();
+            }
         }
 
         static bool ChangeMutexName(string name, byte[] processMemory, out IntPtr baseAddress)
@@ -102,13 +120,13 @@ namespace RunPOEBypassMutex
             var offset = BoyerMooreHorspool.IndexOf(processMemory, original);
             if (offset == -1)
             {
-                Console.WriteLine("[ERROR] Offset for '{0}' is not found.", name);
+                Console.WriteLine("[-] Offset for '{0}' is not found.", name);
                 return false;
             }
 
             baseAddress = _process.MainModule.BaseAddress + offset;
             Console.WriteLine(
-                "BaseAddress {0:X} found for mutex with name {1} (name size: {2})",
+                "[+] BaseAddress {0:X} found for mutex with name {1} (name size: {2})",
                 baseAddress.ToInt64(),
                 name,
                 originalSize);
@@ -124,24 +142,24 @@ namespace RunPOEBypassMutex
             var newBytes = Encoding.Unicode.GetBytes(newName);
             var size = newBytes.Length;
 
-            Console.WriteLine("New mutex name: {0} (size: {1})", newName, newBytes.Length);
+            Console.WriteLine("[+] New mutex name: {0} (size: {1})", newName, newBytes.Length);
 
             if (!ChangeProtection(baseAddress, size, 0x40, out int oldProtectionFlag))
             {
-                ReportError("[1] Unable to change memory protection flag.");
+                ReportError("[-] Unable to change memory protection flag 1.");
                 return false;
             }
 
             var bytesRead = 0;
             if (!Native.WriteProcessMemory(_process.Handle, baseAddress, newBytes, size, ref bytesRead))
             {
-                ReportError("Unable to write a new mutex name.");
+                ReportError("[-] Unable to write a new mutex name.");
                 return false;
             }
 
             if (!ChangeProtection(baseAddress, size, oldProtectionFlag, out _))
             {
-                ReportError("[2] Unable to change memory protection flag.");
+                ReportError("[-] Unable to change memory protection flag 2.");
                 return false;
             }
 
@@ -156,8 +174,24 @@ namespace RunPOEBypassMutex
         static void ReportError(string error)
         {
             _process.Kill();
-            Console.WriteLine("[ERROR] " + error);
+            Console.WriteLine("[-] " + error);
             Console.ReadLine();
+        }
+
+        static void WaitForProcess()
+        {
+            while (true)
+            {
+                try
+                {
+                    var _ = _process.StartTime;
+                    break;
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+            }
         }
     }
 }
